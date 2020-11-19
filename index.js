2c2557c2795193aa77328f6a1e05ef228b7f845e
Sonja Ortmann <so.ortmann@gmail.com>
2020-11-19 13:35:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ function shout(string) { return string.toUpperCase()}
 function whisper(string) {return string.toLowerCase ()}
 function logShout (string) {console.log(string.toUpperCase ())}
 function logWhisper (string) {console.log(string.toLowerCase ())}
-function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) {
-    return "I can't hear you!";
-}
-else if (string === string.toUpperCase()) {
-    return "YES INDEED!";
-} else if (string === string'I love you, Grandma.') {
-  return 'I love you, too.'
+function sayHiToGrandma (string) {
+  var cantUnswer = "I can't hear you!";
+  var yesUnswer = "YES INDEED!";
+  var lovUnswer = "I love you, too.";
+  if (string.toLowerCase(string) === string) {
+    return cantUnswer;
+  }
+  else if (string.toUpperCase(string) === string) {
+    return yesUnswer;
+  }
+  else if ("I love you, Grandma." === string) {
+    return lovUnswer
+  }
 }
